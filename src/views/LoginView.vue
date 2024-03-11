823fa51fee71dd48b8f15612160e338a40c6ea31
BunyakornGoko <bunyakorn.porn@mail.kmutt.ac.th>
2024-03-11 17:40:07 +0700

    Initial Plannet

---
+++ src/views/LoginView.vue
@@ -0,0 +1,33 @@
+<script setup>
+
+</script>
+
+<template>
+  <main class="w-full h-screen grid place-items-center">
+    <div class="flex flex-col">
+      <div>
+        <p class="text-2xl mb-2">Login to Plannet</p>
+        <p>Welcome to a workspace that's secure, powerful, and <br>totally private.</p>
+      </div>
+      <div class="flex flex-col gap-5 mt-6 mb-4">
+        <div>
+        <label for="email"></label>
+        <input type="text" id="email" name="email" placeholder="Email or username" 
+        class="border border-neutral rounded-xl bg-neutral w-96 px-4 py-1">
+      </div>
+      <div>
+        <label for="password"></label>
+        <input type="text" id="password" name="password" placeholder="Password" 
+        class="border border-neutral rounded-xl bg-neutral w-96 px-4 py-1">
+      </div>
+      <div>
+        <button type="button" class="border border-accent rounded-2xl bg-accent w-96 px-4 py-1 text-base-100">Login</button>
+      </div>
+      </div>
+      <div class="">
+        <p class="text-primary"><a href="" class="text-primary">Forgot password?</a></p>
+        <p>Don't have an account? <a href="" class="text-primary">Signup</a></p>
+      </div>
+    </div>
+  </main>
+</template>
