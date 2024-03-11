54e7aa07ab52a08892478ab5310af6f8e17bbeaa
BunyakornGoko <bunyakorn.porn@mail.kmutt.ac.th>
2024-03-11 21:21:30 +0700

    add: auth UI 0.1

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,23 +1,26 @@
 <script setup>
-
+import Logo from '@/components/Logo.vue'
 </script>
 
 <template>
   <main class="w-full h-screen grid place-items-center">
     <div class="flex flex-col">
       <div>
+        <div class="text-primary mb-4">
+          <Logo size="4rem" color="currentColor" />
+        </div>
         <p class="text-2xl mb-2">Login to Plannet</p>
         <p>Welcome to a workspace that's secure, powerful, and <br>totally private.</p>
       </div>
       <div class="flex flex-col gap-5 mt-6 mb-4">
         <div>
         <label for="email"></label>
-        <input type="text" id="email" name="email" placeholder="Email or username" 
+        <input type="text" id="email" placeholder="Email or username" 
         class="border border-neutral rounded-xl bg-neutral w-96 px-4 py-1">
       </div>
       <div>
         <label for="password"></label>
-        <input type="text" id="password" name="password" placeholder="Password" 
+        <input type="text" id="password" placeholder="Password" 
         class="border border-neutral rounded-xl bg-neutral w-96 px-4 py-1">
       </div>
       <div>
@@ -26,7 +29,7 @@
       </div>
       <div class="">
         <p class="text-primary"><a href="" class="text-primary">Forgot password?</a></p>
-        <p>Don't have an account? <a href="" class="text-primary">Signup</a></p>
+        <p>Don't have an account? <a href="" class="text-primary">Sign up</a></p>
       </div>
     </div>
   </main>
---
+++ src/views/RegisterView.vue
@@ -0,0 +1,45 @@
+<script setup>
+import Logo from '@/components/Logo.vue'
+</script>
+ 
+<template>
+  <main class="w-full h-screen grid place-items-center">
+    <div class="flex flex-col">
+      <div>
+        <div class="text-primary mb-4">
+          <Logo size="4rem" color="currentColor" />
+        </div>
+        <p class="text-2xl mb-2">Sign up for Plannet</p>
+        <p>Welcome to a workspace that's secure, powerful, and <br>totally private.</p>
+      </div>
+      <div class="flex flex-col gap-5 mt-6 mb-4">
+        <div>
+          <label for="username"></label>
+          <input type="text" id="username" placeholder="Username" 
+          class="border border-neutral rounded-xl bg-neutral w-96 px-4 py-1">
+        </div>
+        <div>
+          <label for="email"></label>
+          <input type="email" id="email" placeholder="Email" 
+          class="border border-neutral rounded-xl bg-neutral w-96 px-4 py-1">
+        </div>
+        <div>
+          <label for="password"></label>
+          <input type="text" id="password" placeholder="Password" 
+          class="border border-neutral rounded-xl bg-neutral w-96 px-4 py-1">
+        </div>
+        <div>
+          <label for="confirmpassword"></label>
+          <input type="text" id="confirmpassword" name="confirmpassword" placeholder="Confirm password" 
+          class="border border-neutral rounded-xl bg-neutral w-96 px-4 py-1">
+        </div>
+        <div>
+          <button type="button" class="border border-accent rounded-2xl bg-accent w-96 px-4 py-1 text-base-100">Sign up</button>
+        </div>
+      </div>
+      <div>
+        <p>Do you have an account? <a href="" class="text-primary">Login</a></p>
+      </div>
+    </div>
+  </main>
+</template>
