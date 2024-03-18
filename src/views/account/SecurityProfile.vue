aa91d74a58c56eeb2ac34ee05719124d176e8e91
Nongmook101 <siriwan.hosa@kmutt.ac.th>
2024-03-18 20:26:41 +0700

    chore: change name variable

--- src/views/account/SecurityProfile.vue
+++ src/views/account/SecurityProfile.vue
@@ -4,7 +4,7 @@ import { checkPassword } from '../../../libs/checkPassword';
 import { ref } from 'vue';
 const password = ref('')
 
-const isOk = computed(()=>{
+const isPasswdValid = computed(()=>{
     console.log(checkPassword(password.value));
     return checkPassword(password.value)
 })
@@ -13,7 +13,7 @@ const isOk = computed(()=>{
 <template>
 <div class="flex flex-col pt-5 gap-3 pl-5 w-screen max-w-sm">
     <input type="text" class="input input-bordered w-100" @input="checkPassword(password)" placeholder="Enter new password" v-model="password">
-    <p class="text-red-400" v-show=" !isOk.isPasswordSecure">
+    <p class="text-red-400" v-show=" !isPasswdValid.isPasswordSecure">
     <p v-for ="warn in isOk.warning">{{warn }}</p>
     </p>
        
