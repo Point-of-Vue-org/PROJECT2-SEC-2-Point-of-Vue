59109e81b61b3d736bb182789eecb1ee30464519
22p21s0045 <monkeytitle@hotmail.com>
2024-03-22 21:18:42 +0700

    add: refactor with library v1

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -1,25 +1,24 @@
 <script setup>
 import Logo from '@/components/Logo.vue'
-import { reactive, ref, watch } from 'vue'
+import { computed, reactive, ref, watch } from 'vue'
 import { useToastStore } from '@/stores/toast'
 import { isEmailExist, isUsernameExist, register } from '../../libs/userManagement';
 import { useRouter } from 'vue-router'
+import { checkPassword } from '../../libs/passwordValidation';
 
 const toastStore = useToastStore()
 const router = useRouter()
-
 const errorMsg = ref('')
+const isLoading = ref(false)
+const result = computed(()=>{
+  
+  return checkPassword(password.value)
+})
 const username = ref('')
 const email = ref('')
 const password = ref('')
 const confirmPassword = ref('')
-const isLoading = ref(false)
-const digitChecked = ref(false)
-const letterChecked = ref(false)
-const capLetterChecked = ref(false)
-const specialCharChecked = ref(false)
-const passwdWarning = ref('')
-const passwdChecked = ref(false)
+
 
 
 const handleSubmitRegister = async () => {
@@ -53,44 +52,6 @@ watch([password, confirmPassword], () => {
   }
 })
 
-watch( password, (newPassword) => {
-  
-    if (newPassword.length < 8) {
-      passwdWarning.value = "Password must be at least 8 characters long"
-    } else {
-      passwdWarning.value = ''
-    }
-
-    if (/^[\x20-\x7E]+$/.test(newPassword) && newPassword !== '') {
-      passwdChecked.value = true
-    } else {
-      passwdChecked.value = false
-    }
-    
-    if (/[\x61-\x7A]/.test(newPassword)) {
-      letterChecked.value = true
-    } else {
-      letterChecked.value = false
-    }
-
-    if (/[\x41-\x5A]/.test(newPassword)) {
-      capLetterChecked.value = true
-    } else {
-      capLetterChecked.value = false
-    }
-
-    if (/[\x30-\x39]/.test(newPassword)) {
-      digitChecked.value = true
-    } else {
-      digitChecked.value = false
-    }
-
-    if (/[\x21-\x2F\x3A-\x40\x5B-\x60\x7B-\x7E]/.test(newPassword)) {
-      specialCharChecked.value = true
-    } else {
-      specialCharChecked.value = false
-    }
-})
 
 </script>
 
@@ -105,19 +66,15 @@ watch( password, (newPassword) => {
         <p>Welcome to a workspace that's secure, powerful, and totally private.</p>
       </div>
       <div class="flex flex-col gap-5 mt-6 mb-4">
-        <div v-show="errorMsg" class="text-error">{{ errorMsg }}</div>
+        <div v-show="!result.value?.isPasswordSecure" class="text-error">{{ result.value?.warning }}</div>
         <input v-model="username" type="text" placeholder="Username" autocomplete="username" title="Username" autofocus
           required class="input bg-neutral rounded-xl w-96" />
         <input v-model="email" type="email" placeholder="Email" autocomplete="email" title="Email" required
           class="input bg-neutral rounded-xl w-96">
         <input v-model="password" type="password" placeholder="Password" autocomplete="new-password" title="Password"
           required class="input bg-neutral rounded-xl w-full" />
-          <div v-show="passwdWarning" class="text-error">Warning: {{ passwdWarning }}</div>
-          <div>passwdChecked: {{ passwdChecked }}</div>
-          <div>Letters: {{ letterChecked }}</div>
-          <div>Capital Letters: {{ capLetterChecked }}</div>
-          <div>Digits: {{ digitChecked }}</div>
-          <div>Special Cahracter: {{ specialCharChecked }}</div>
+          <div v-show="!result.isPasswordSecure" class="text-error">Warning: <p v-for="(warn,index) in result.warning" :key="index">{{ warn }}</p></div>
+          <div class="text-red-400">{{ errorMsg }}</div>
         <input v-model="confirmPassword" type="password" placeholder="Confirm password" autocomplete="new-password"
           title="Confirm password" required class="input bg-neutral rounded-xl w-full" />
         <button type="submit" class="btn rounded-full btn-accent text-base-100">Sign up</button>
