872b85899d7118c210aa5192f03ac87256473c2e
6MA-606 <zyxma.6666@hotmail.com>
2024-03-25 18:55:25 +0700

    update: user profile ui v0.2.0

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -18,6 +18,8 @@ const user = ref(new User())
 const postPlans = ref([])
 const postCount = ref(0)
 const upVoteCount = ref(0)
+const sortBy = ref('latest')
+const sortOpen = ref(false)
 
 // onBeforeMount(
 //   async () => {
@@ -84,17 +86,41 @@ watch(route, () => {
             class="w-full h-full object-cover"
           />
         </div>
-        <div class="h-[8rem] bg-base-200 flex flex-col mt-4 translate-x-[16rem]">
-          <div class="text-2xl font-bold">{{ user.nickname }}</div>
-          <div class=" opacity-50">{{ '@' + user.username }}</div>
-          <div class="flex gap-4">
-            <div class="text-sm">{{ postCount }} Posts</div>
-            <div class="text-sm">{{ upVoteCount }} Upvotes</div>
+        <div class="h-[8rem] bg-base-200 flex flex-col gap-4 mt-4 translate-x-[16rem]">
+          <div class="flex flex-col">
+            <div class="text-2xl font-bold">{{ user.nickname }}</div>
+            <div class=" opacity-50">{{ '@' + user.username }}</div>
+          </div>
+          <div class="flex gap-2">
+            <div class="text-sm"><span class="font-bold">{{ postCount }}</span> Posts</div>
+            <div class="text-sm"><span class="font-bold text-primary">{{ upVoteCount }}</span> Upvotes</div>
           </div>
         </div>
       </div>
-      <div class="bg-base-200 rounded-2xl border border-neutral w-full h-80 overflow-hidden flex flex-col">
-
+      <nav class="flex items-center gap-4">
+        <button class="btn btn-sm border border-neutral">{{ user.nickname }} 's post</button>
+        <div class="select-none">
+          <span>Sort by </span>
+          <span class="dropdown">
+            <div tabindex="0" role="button" @click="sortOpen = !sortOpen" class="flex items-center gap-2 text-primary">{{ sortBy }} <Icon iconName="chevron-down" /></div>
+            <ul tabindex="0" class="dropdown-content z-[1] menu p-2 shadow bg-base-100 border border-neutral rounded-box w-52">
+              <li @click="sortBy = 'latest'" class="hover:bg-neutral cursor-pointer p-2 rounded-md">latest</li>
+              <li @click="sortBy = 'oldest'" class="hover:bg-neutral cursor-pointer p-2 rounded-md">oldest</li>
+              <li @click="sortBy = 'most upvotes'" class="hover:bg-neutral cursor-pointer p-2 rounded-md">most upvotes</li>
+              <li @click="sortBy = 'most comments'" class="hover:bg-neutral cursor-pointer p-2 rounded-md">most comments</li>
+            </ul>
+          </span>
+        </div>
+      </nav>
+      <div class="bg-base-200 rounded-2xl border border-neutral w-full min-h-[32rem] h-auto overflow-hidden flex flex-col items-center">
+        <div class="h-6"></div>
+        <PlanContainer>
+          <PlanCard
+            v-for="plan in postPlans"
+            :key="plan.id"
+            :planData="plan"
+          />
+        </PlanContainer>
       </div>
     </section>
   </main>
