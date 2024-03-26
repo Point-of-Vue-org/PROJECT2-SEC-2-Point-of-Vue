09ec425c8fd6dea07ca9e55ed5a5467dcada2022
22p21s0045 <monkeytitle@hotmail.com>
2024-03-26 08:39:29 +0700

    add: todo can add

--- src/views/EditDraftPlanView.vue
+++ src/views/EditDraftPlanView.vue
@@ -11,6 +11,8 @@ import Icon from "@/components/Icon.vue";
 import { DailyTask } from "../../classes/DailyTask";
 import { getPlans, createOrUpdatePlan,getPlanBy } from "../../libs/planManagement";
 import ListContainer from "@/components/ListContainer.vue";
+import { HourlyTask } from "../../classes/HourlyTask";
+import { Todo } from "../../classes/Todo";
 
 // const JSON_SERVER_URI = import.meta.env.VITE_SERVER_URI || 'http://localhost:5000'
 const router = useRouter();
@@ -18,11 +20,19 @@ const route = useRoute();
 const toastStore = useToastStore();
 const userStore = useUserStore();
 const draftPlan = ref(new BasePlan());
-const newDraftPlan = ref('')
+const hourlyTask = ref(new HourlyTask())
+let newDraftPlan = ref('')
+// let {  dailyTasks } = newDraftPlan.value.dailyTask
+
+watch(newDraftPlan, async(newValue) => {
+  console.log(newDraftPlan.value);
+  console.log(newValue);
+ 
+},{deep:true})
 // const dailyTasks = ref([new DailyTask()]);
 onMounted(async () => {
   let id = route.params.id;
-  newDraftPlan = await getPlanBy('id',id,'draft')
+  newDraftPlan.value = await getPlanBy('id',id,'draft')
   console.log(newDraftPlan);
 
   // let id = await userStore.userData.id
@@ -31,7 +41,28 @@ onMounted(async () => {
   
 });
 function handleAddDraftPlan() {
-  draftPlan.value.dailyTasks.push(new BasePlan());
+  const newDailyTask = new BasePlan();
+  newDailyTask.dailyTasks = []; // Initialize dailyTasks as an empty array
+  newDraftPlan.value.dailyTasks.push(newDailyTask);
+}
+function addHourlyTask(index, task) {
+  const newHourlyTask = new HourlyTask();  
+  newDraftPlan.value.dailyTasks[index].dailyTasks.push(newHourlyTask);
+  
+
+}
+function addTodo(dailyIndex, hourlyIndex) {
+  const newTodo = {...new Todo()}
+  const todos = [...newDraftPlan.value.dailyTasks[dailyIndex].dailyTasks[hourlyIndex].todos] 
+todos.push(newTodo)
+newDraftPlan.value.dailyTasks[dailyIndex].dailyTasks[hourlyIndex].todos = todos
+  
+  
+  
+  console.log(dailyIndex);
+  console.log(hourlyIndex);
+  
+
 }
 onBeforeMount(async () => {
   const { isTokenValid, userId } = await validateToken();
@@ -73,13 +104,13 @@ const handleLogout = async () => {
     <section class="flex-auto flex justify-center">
       <div class="w-[85%]">
         <input
-          v-model="draftPlan.title"
+          v-model="newDraftPlan.title"
           type="text"
           placeholder="Title here"
           class="text-7xl bg-transparent focus:outline-none w-full p-2 placeholder:opacity-50"
         />
         <textarea
-          v-model="draftPlan.description"
+          v-model="newDraftPlan.description"
           placeholder="Description here"
           class="text-2xl bg-transparent focus:outline-none w-full p-2 placeholder:opacity-50"
         ></textarea>
@@ -91,15 +122,78 @@ const handleLogout = async () => {
         <div
           class="w-full pt-3 justify-center flex flex-col gap-4 items-center"
         >
-          <div v-for="(dailyTask, index) in draftPlan.dailyTasks" :key="index" class="w-full">
+          <div v-for="(dailyTask, dailyindex) in newDraftPlan.dailyTasks" :key="dailyindex" class="w-full">
           <div alt = "Daily task" class="flex flex-col">
             <div class="flex flex-row">
-              <p class="w-1/6 flex items-center">Day {{ index + 1 }}</p>
+              <p class="w-1/6 flex items-center">Day {{ dailyindex + 1 }}</p>
+              
               <input type="text" placeholder="Task title" class="input input-bordered w-full" v-model="dailyTask.title"/>
             </div>
+            <div>
+              <div class="flex justify-end mt-4">
+                <button class="btn " @click="addHourlyTask(dailyindex)">Add holytask</button>
+              </div>
+          <div class="bg-stone-600 p-6 rounded mt-6" alt = "hourly task" v-for="(hourlyTask,hourlyindex) in newDraftPlan.dailyTasks[dailyindex].dailyTasks" :key="hourlyindex">
+
+            
+            <div class="flex gap-3 bg-slate-400 rounded p-5 mt-6">
+             
+              <input type="time" class="rounded p-3 bg-blue-100 text-black" alt="start" v-model="hourlyTask.start" />
+              <input
+                type="time"
+                class="rounded p-3 bg-red-300-100 text-black"
+                alt = 'stop'
+                v-model="hourlyTask.stop"
+                
+
+              />
+              <input
+                class="input input-bordered w-1/2"
+                v-model="hourlyTask.title"
+                placeholder="please type your daily task"
+              />
+              <Icon iconName="trash3-fill" class="flex items-center" />
+            </div>
+            <div class="mt-5 w-full flex flex-row gap-4">
+              <textarea
+                class="textarea-bordered rounded w-1/2 max-h-40"
+                placeholder="description"
+                v-model="hourlyTask.description"
+              ></textarea>
+
+              <div class="w-1/2 border-l-2 border-indigo-400 p-4">
+                <div class="flex flex-row justify-between">
+                  <p>Task</p>
+                  <button class="btn  ml-auto" @click = addTodo(dailyindex,hourlyindex)>Add</button>
+                </div>
+                <div class="flex flex-col pt-2">
+                  <div class="flex flex-col gap-3">
+                   
+                      <div v-for="(todo,todoIndex) in newDraftPlan.dailyTasks[dailyindex].dailyTasks[hourlyindex].todos" :key="todoIndex" class="flex flex-row gap-4">
+                        <input 
+                        type="text"
+                        class="input input-bordered w-full"
+                        placeholder="Subtask"
+                        v-model="todo.description"
+                        :key="todoIndex"
+                       
+                      />
+                      <Icon iconName="trash3-fill" class="flex items-center" />
+{{ console.log(newDraftPlan.dailyTasks[dailyindex]) }}
+                      </div>
+                      
+                   
+                  </div>
+                </div>
+              </div>
+              
+            </div>
+          </div>
+       
           
             
           </div>
+        </div>
           </div>
         </div>
       </div>
