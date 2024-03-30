66979b892e51c653ec5331c53f3311b39f79a04e
22p21s0045 <monkeytitle@hotmail.com>
2024-03-31 01:10:23 +0700

    add: search-page-v1.0.1

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,10 +7,12 @@ import PlannetLayout from '@/components/PlannetLayout.vue'
 import SortMethodSelector from '@/components/SortMethodSelector.vue'
 import { sortObject } from '../../libs/utils'
 import { useRoute, useRouter } from 'vue-router'
+import { getUsers } from '../../libs/userManagement'
 const route = useRoute()
 const router = useRouter()
 const postPlans = ref([])
 const sortBy = ref(['postDate', 'desc'])
+const users = ref([])
 const sortAblePostPlans = computed(() => {
   return sortObject(postPlans.value, sortBy.value[0], sortBy.value[1])
 })
@@ -35,27 +37,42 @@ watch(
   () => route.query.search,
   async (value) => {
     if(value){
+      if(/^@/.test(value)){
+        console.log(value.slice(1))
+        const allUsers = await getUsers(true)
+        const searchPattern = new RegExp(value.slice(1), 'i')
+        users.value = allUsers.filter(user => {
+          const temp = searchPattern.test(user.username)
+          return temp
+        })
+        console.log(users.value)
+      }
+      else{
       console.log("Hi");
       const allPostPlans = await getPlans('post')
-      const searchPattern = new RegExp(route.query.search, 'ig')
+      const searchPattern = new RegExp(route.query.search, 'i')
       postPlans.value = allPostPlans.filter(postPlan => {
-        const temp = searchPattern.test(postPlan.title)
+      const temp = searchPattern.test(postPlan.title)
         console.log(temp);
         return temp
       })
+      }
+      
     } else {
       router.push('/')
+      users.value = []
       postPlans.value = await getPlans('post')
     }
-  }
+  },
+  { immediate: true }
 )
 </script>
 
 <template>
   <PlannetLayout @submitSearch="handleSubmitSearch">
-    <!-- {{ route.query?.search }} -->
     <div class="flex flex-col items-center">
       <div class="w-fit">
+        Search: {{ route.query?.search }}
         <div class="h-24 w-full flex justify-start items-center">
           <!-- <RouterLink to="/plan/create" class="btn btn-outline">Add your plan</RouterLink> -->
           <SortMethodSelector
@@ -70,9 +87,17 @@ watch(
             @sortOptionSelect="sortBy = $event"
           />
         </div>
-        <PlanContainer>
+        <PlanContainer v-if="users.length === 0">
           <PlanCard v-for="plan in sortAblePostPlans" :key="plan.id" :postPlan="plan" />
         </PlanContainer>
+        <div v-else>
+          <div v-for="user in users" :key="user.id" class="flex items-center">
+            <div class="flex-1">
+              <div class="text-xl font-bold">{{ user.username }}</div>
+            </div>
+          </div>
+
+        </div>
       </div>
     </div>
     <div class="h-16"></div>
